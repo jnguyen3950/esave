abd858b4f2ff49afdf54cd27906ad94be6fa8916
Justin Nguyen <jnguyen3950@gmail.com>
2016-04-27 10:51:45 -0700

    #1 Search button will now pass item term to request. View model still doesn't change.

--- public/factory/user.service.js
+++ public/factory/user.service.js
@@ -5,8 +5,8 @@ app.factory('userService', userService);
 userService.$inject = ['$http'];
 
 function userService($http) {
-  function search() {
-    return $http.post('http://localhost:1337/search');
+  function search(term) {
+    return $http.post('http://localhost:1337/search/' + term);
   }
 
   function calculateDistance() {
--- public/search/search.controller.js
+++ public/search/search.controller.js
@@ -6,8 +6,16 @@ app.$inject = ['userService'];
 
 function search(userService) {
   var vm = this;
-  var promise = userService.search();
+  var promise = userService.search("Harry Potter");
   promise.then(function(result) {
     vm.list = result.data.findItemsByKeywordsResponse[0].searchResult[0].item;
   });
+
+  vm.newSearch = function(term) {
+    var data = term;
+    var promise = userService.search(term);
+    promise.then(function(result) {
+      vm.list = result.data.findItemsByKeywordsResponse[0].searchResult[0].item;
+    });
+  }
 }
