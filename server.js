7577cbc37319074c9af06e1848084937fd5e05a3
Justin Nguyen <jnguyen3950@gmail.com>
2016-04-29 15:24:06 -0700

    #6 Thumb image links are now return the correct information.

--- server.js
+++ server.js
@@ -35,23 +35,6 @@ app.get('/search/:term/:zip/:distance/:minPrice/:maxPrice', function(req, res) {
   }
 });
 
-// app.get('/info/:productId', function(req, res) {
-//   var queryParam = {
-//     'productId': "3400594"
-//   }
-//
-//   request('http://svcs.ebay.com/services/search/FindingService/v1?'
-//   + 'OPERATION-NAME=findItemsByProduct&SERVICE-VERSION=1.13.0'
-//   + '&SECURITY-APPNAME=' + appID
-//   + '&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&'
-//   + 'paginationInput.entriesPerPage=2&'
-//   + 'productId.@type=ReferenceID&'
-//   + querystring.stringify(queryParam), function(err, response, body) {
-//     if(err) res.sendStatus(err);
-//     res.send(body);
-//   });
-// });
-
 app.get('/info/:itemId', function(req, res) {
   console.log(req.params.itemId);
   var queryParam = {
