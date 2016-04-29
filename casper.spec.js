21f4e75debf3b485be352615b04d7b949353d9da
Justin Nguyen <jnguyen3950@gmail.com>
2016-04-29 09:29:52 -0700

    Modified casper.spec.js

--- casper.spec.js
+++ casper.spec.js
@@ -2,7 +2,7 @@ casper.test.begin('eSave casper test suit', 2, function suite(test) {
   casper.start("http://localhost:" + 8080, function() {
     test.assertTitle("eSave", "eSave title is found.");
   })
-  casper.wait(200, function() {
+  casper.waitForSelector('#searchResult', function() {
     casper.echo(this.page.content);
     this.capture('Home-Page.png', {
       top: 0,
@@ -14,14 +14,15 @@ casper.test.begin('eSave casper test suit', 2, function suite(test) {
   casper.thenEvaluate(function() {
     document.querySelector('#searchResult').click();
   });
-  casper.wait(500, function() {
+  casper.waitForSelector('#searchButton', function() {
+    casper.echo(this.page.content);
     test.assertUrlMatch('http://localhost:' + 8080 + '/#/search', "eSave Url match.");
   });
   casper.then(function() {
     this.sendKeys('input#newTerm', 'Lego');
     this.clickLabel('Search', 'button');
   });
-  casper.wait(1000, function() {
+  casper.waitForSelector('form', function() {
     this.capture('Search-Page.png', {
       top: 0,
       left: 0,
