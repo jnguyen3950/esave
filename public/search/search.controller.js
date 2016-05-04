4c037038acb9ace80e9b84c7874e07172a5ef6dc
Justin Nguyen <jnguyen3950@gmail.com>
2016-05-04 10:13:02 -0700

    #2 Searching method is updated and now showing more descriptive detail about searching location.

--- public/search/search.controller.js
+++ public/search/search.controller.js
@@ -5,28 +5,29 @@ app.controller('searchController', search);
 function search($http, Search) {
   var vm = this;
 
-  vm.newSearch = function(term, zip, dist, minPrice, maxPrice) {
-    this.zip = zip;
-    this.dist = dist || 25;
-    this.minPrice = minPrice || 0;
-    this.maxPrice = maxPrice || 100;
-    var promise = new Promise(function(resolve, reject) {
-      window.navigator.geolocation.getCurrentPosition(function(pos){
+  vm.init = function() {
+    window.navigator.geolocation.getCurrentPosition(function(pos){
+      $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+pos.coords.latitude+','+pos.coords.longitude+'&sensor=true').then(function(res){
         $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+pos.coords.latitude+','+pos.coords.longitude+'&sensor=true').then(function(res){
-          $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+pos.coords.latitude+','+pos.coords.longitude+'&sensor=true').then(function(res){
-            var zipcode = res.data.results[0].address_components[6].long_name;
-            vm.zipcode = zipcode;
-            resolve(zipcode);
-          });
+          var zipcode = res.data.results[0].address_components[6].long_name;
+          vm.currentPlace = res.data.results[0].address_components[2].short_name + ', ' + res.data.results[0].address_components[4].short_name + ' ' + res.data.results[0].address_components[6].short_name;
+          vm.zipcode = zipcode;
         });
       });
     });
+  }
 
-    promise.then(function(zipcode) {
-      var items = Search.items(term, this.zip, this.dist, this.minPrice, this.maxPrice);
-      items.then(function(result) {
-        vm.list = result.data.findItemsByKeywordsResponse[0].searchResult[0].item;
-      });
+  vm.newSearch = function(term, zip, dist, minPrice, maxPrice) {
+    this.zip = zip || vm.zipcode;
+    this.dist = dist || 25;
+    this.minPrice = minPrice || 0;
+    this.maxPrice = maxPrice || 100;
+
+    vm.currentSearch = this.zip;
+
+    var items = Search.items(term, this.zip, this.dist, this.minPrice, this.maxPrice);
+    items.then(function(result) {
+      vm.list = result.data.findItemsByKeywordsResponse[0].searchResult[0].item;
     });
   }
 
--- public/search/search.factory.js
+++ public/search/search.factory.js
@@ -6,11 +6,11 @@ function search($http) {
   function items(term, zip, dist, minPrice, maxPrice) {
     currentURL = window.location.origin;
 
-    this.zip = zip.value || 92660;
-    this.dist = dist.value || 25;
-    this.minPrice = minPrice.value || 10;
-    this.maxPrice = maxPrice.value || 100;
-    return $http.get(currentURL + '/search/'+term+'/'+this.zip+'/'+this.dist+'/'+this.minPrice+'/'+this.maxPrice);
+    this.zip = zip;
+    this.dist = dist || 25;
+    this.minPrice = minPrice || 10;
+    this.maxPrice = maxPrice || 100;
+    return $http.get(currentURL+'/search/'+term+'/'+this.zip+'/'+this.dist+'/'+this.minPrice+'/'+this.maxPrice);
   }
   return {
     items: items
